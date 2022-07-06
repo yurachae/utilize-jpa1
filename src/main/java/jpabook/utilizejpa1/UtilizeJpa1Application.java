6af95e5411fdbf4be5e3d174ab3ee3b1439828cd
YURACHAE\yurachae <yura920501@gmail.com>
2022-07-06 13:28:26 +0900

    Initial commit TEST

---
+++ src/main/java/jpabook/utilizejpa1/UtilizeJpa1Application.java
@@ -0,0 +1,13 @@
+package jpabook.utilizejpa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilizeJpa1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilizeJpa1Application.class, args);
+    }
+
+}
---
+++ src/test/java/jpabook/utilizejpa1/UtilizeJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.utilizejpa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilizeJpa1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
