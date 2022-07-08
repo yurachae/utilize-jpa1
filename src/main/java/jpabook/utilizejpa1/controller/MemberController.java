08f20aba19e802c7e598bc4e6f0a4227a042170d
YURACHAE\yurachae <yura920501@gmail.com>
2022-07-08 17:27:43 +0900

    회원 등록 폼, 회원 등록 개발

---
+++ src/main/java/jpabook/utilizejpa1/controller/MemberController.java
@@ -0,0 +1,36 @@
+package jpabook.utilizejpa1.controller;
+
+import jpabook.utilizejpa1.domain.Address;
+import jpabook.utilizejpa1.domain.Member;
+import jpabook.utilizejpa1.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String creteFrom(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm memberForm) {
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
+
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
